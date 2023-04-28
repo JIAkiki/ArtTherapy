fcd8798a3f817bf7bfc86dec50a54adb9e059546
Jenny Wu <122882512+JIAkiki@users.noreply.github.com>
2023-04-28 23:57:50 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,36 @@
-let currentColor = 'black';
-let currentSize = 4;
+let colorPicker;
+let brushSizeSlider;
 
 function setup() {
     createCanvas(windowWidth, windowHeight - 50);
     background(255);
+
+    const toolbar = select('#toolbar');
+
+    colorPicker = createColorPicker('black');
+    colorPicker.parent(toolbar);
+
+    createElement('br').parent(toolbar);
+    createElement('br').parent(toolbar);
+
+    createSpan('Brush size: ').parent(toolbar);
+    brushSizeSlider = createSlider(1, 20, 4);
+    brushSizeSlider.parent(toolbar);
+
+    createElement('br').parent(toolbar);
+    createElement('br').parent(toolbar);
+
+    const clearButton = createButton('Clear');
+    clearButton.mousePressed(clearCanvas);
+    clearButton.parent(toolbar);
 }
 
 function draw() {
     if (mouseIsPressed) {
-        stroke(currentColor);
-        strokeWeight(currentSize);
+        stroke(colorPicker.color());
+        strokeWeight(brushSizeSlider.value());
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
-}
-
-function changeColor(color) {
-    currentColor = color;
-}
-
-function changeSize(size) {
-    currentSize = size;
 }
 
 function clearCanvas() {
