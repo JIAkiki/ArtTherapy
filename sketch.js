3e11098e1ab82ad03ab8dbb2839eb026a9b47f20
Jenny Wu <122882512+JIAkiki@users.noreply.github.com>
2023-04-29 00:04:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+let video;
+let faceapi;
+let emotions;
+
 let colorPicker;
 let brushSizeSlider;
 
@@ -23,9 +27,35 @@ function setup() {
     const clearButton = createButton('Clear');
     clearButton.mousePressed(clearCanvas);
     clearButton.parent(toolbar);
+
+    // Set up video capture and Face API
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    video.hide();
+
+    const faceapi_options = {
+        withLandmarks: false,
+        withDescriptors: false,
+        withExpressions: true,
+    };
+    faceapi = ml5.faceApi(video, faceapi_options, onFaceapiReady);
 }
 
 function draw() {
+    if (emotions) {
+        let dominantEmotion = Object.keys(emotions).reduce((a, b) => (emotions[a] > emotions[b] ? a : b));
+
+        if (dominantEmotion === 'happy') {
+            background(255, 255, 100);
+        } else if (dominantEmotion === 'sad') {
+            background(100, 100, 255);
+        } else if (dominantEmotion === 'angry') {
+            background(255, 100, 100);
+        } else {
+            background(255);
+        }
+    }
+
     if (mouseIsPressed) {
         stroke(colorPicker.color());
         strokeWeight(brushSizeSlider.value());
@@ -35,4 +65,22 @@ function draw() {
 
 function clearCanvas() {
     background(255);
+}
+
+function onFaceapiReady() {
+    console.log('Face API model ready!');
+    faceapi.detect(gotFaces);
+}
+
+function gotFaces(error, result) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    if (result) {
+        emotions = result[0].expressions;
+    }
+
+    faceapi.detect(gotFaces);
 }
