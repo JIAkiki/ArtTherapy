f853a07c6360779208f503f0e6715fd334370fc9
Jenny Wu <122882512+JIAkiki@users.noreply.github.com>
2023-04-29 03:12:54 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,87 +1,89 @@
 let video;
-let faceapi;
+let net;
 let emotions;
 
 let colorPicker;
 let brushSizeSlider;
 
-function setup() {
-    createCanvas(windowWidth, windowHeight - 50);
-    background(255);
+async function setup() {
+  createCanvas(windowWidth, windowHeight - 50);
+  background(255);
 
-    const toolbar = select('#toolbar');
+  // Your existing toolbar code...
+  const toolbar = select('#toolbar');
 
-    colorPicker = createColorPicker('black');
-    colorPicker.parent(toolbar);
+  colorPicker = createColorPicker('black');
+  colorPicker.parent(toolbar);
 
-    createElement('br').parent(toolbar);
-    createElement('br').parent(toolbar);
+  createElement('br').parent(toolbar);
+  createElement('br').parent(toolbar);
 
-    createSpan('Brush size: ').parent(toolbar);
-    brushSizeSlider = createSlider(1, 20, 4);
-    brushSizeSlider.parent(toolbar);
+  createSpan('Brush size: ').parent(toolbar);
+  brushSizeSlider = createSlider(1, 20, 4);
+  brushSizeSlider.parent(toolbar);
 
-    createElement('br').parent(toolbar);
-    createElement('br').parent(toolbar);
+  createElement('br').parent(toolbar);
+  createElement('br').parent(toolbar);
 
-    const clearButton = createButton('Clear');
-    clearButton.mousePressed(clearCanvas);
-    clearButton.parent(toolbar);
+  const clearButton = createButton('Clear');
+  clearButton.mousePressed(clearCanvas);
+  clearButton.parent(toolbar);
 
-    // Set up video capture and Face API
-    video = createCapture(VIDEO);
-    video.size(width, height);
-    video.hide();
+  // Set up video capture
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
 
-    const faceapi_options = {
-    withLandmarks: false,
-    withDescriptors: false,
-    withExpressions: true,
-    model: 'FaceLandmark68TinyNet'
-    };
-    faceapi = ml5.faceApi(video, faceapi_options, onFaceapiReady);
+  // Load face-api.js model
+  await loadFaceApiModel();
+  net = new faceapi.FaceLandmark68Net();
+  detectFaces();
+}
+
+async function loadFaceApiModel() {
+  const MODEL_URL = 'https://unpkg.com/@vladmandic/face-api@1.3.3/model';
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+  await faceapi.loadFaceExpressionModel(MODEL_URL);
+  console.log('Face API model ready!');
+}
+
+async function detectFaces() {
+  const detections = await faceapi.detectAllFaces(video.elt, new faceapi.SsdMobilenetv1Options()).withFaceExpressions();
+
+  if (detections.length > 0) {
+    emotions = detections[0].expressions;
+  }
+
+  setTimeout(() => detectFaces(), 100);
 }
 
 function draw() {
-    if (emotions) {
-        let dominantEmotion = Object.keys(emotions).reduce((a, b) => (emotions[a] > emotions[b] ? a : b));
+  if (emotions) {
+    let dominantEmotion = Object.keys(emotions).reduce((a, b) => (emotions[a] > emotions[b] ? a : b));
 
-        if (dominantEmotion === 'happy') {
-            background(255, 255, 100);
-        } else if (dominantEmotion === 'sad') {
-            background(100, 100, 255);
-        } else if (dominantEmotion === 'angry') {
-            background(255, 100, 100);
-        } else {
-            background(255);
-        }
+    if (dominantEmotion === 'happy') {
+      background(255, 255, 100);
+    } else if (dominantEmotion === 'sad') {
+      background(100, 100, 255);
+    } else if (dominantEmotion === 'angry') {
+      background(255, 100, 100);
+    } else {
+      background(255);
     }
+  }
 
-    if (mouseIsPressed) {
-        stroke(colorPicker.color());
-        strokeWeight(brushSizeSlider.value());
-        line(mouseX, mouseY, pmouseX, pmouseY);
-    }
+  if (mouseIsPressed) {
+    stroke(colorPicker.color());
+    strokeWeight(brushSizeSlider.value());
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
 
 function clearCanvas() {
-    background(255);
+  background(255);
 }
 
-function onFaceapiReady() {
-    console.log('Face API model ready!');
-    faceapi.detect(gotFaces);
-}
-
-function gotFaces(error, result) {
-    if (error) {
-        console.error(error);
-        return;
-    }
-
-    if (result) {
-        emotions = result[0].expressions;
-    }
-
-    faceapi.detect(gotFaces);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight - 50);
+  background(255);
 }
