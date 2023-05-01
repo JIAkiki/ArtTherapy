c6f324a859958180fbfedbce04a850212d53f7bd
Jenny Wu <122882512+JIAkiki@users.noreply.github.com>
2023-05-02 00:43:37 +0100

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -3,11 +3,15 @@ const colors = {
   sad: "#3498db",
   neutral: "#95a5a6",
 };
-
+let canvas;
 let particles = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  canvas.style('background-color', 'rgba(0,0,0,0)');
+  
   for (let i = 0; i < numParticles; i++) {
     particles[i] = new Particle(random(width), random(height));
   }
